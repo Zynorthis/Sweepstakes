eb16f633ca8802d675ff762c91be48e2b6862a43
Jacob Taylor <jacobtaylor727@outlook.com>
2018-12-13 17:04:20 -0600

    Project Initalization - Created all base classes need for project. Declared Contestant member variables. Declared Sweepstakes member methods.

---
+++ sweepstakes/Contestant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweepstakes
+{
+    class Contestant
+    {
+        private string firstName;
+        private string lastName;
+        private string emailAddress;
+        public int registrationNumber;
+        public string FirstName { get { return firstName; } private set { firstName = value; } }
+        public string LastName { get { return lastName; } private set { lastName = value; } }
+        public string EmailAddress { get { return emailAddress; } private set { emailAddress = value; } }
+
+        public Contestant()
+        {
+
+        }
+    }
+}
---
+++ sweepstakes/Sweepstakes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweepstakes
+{
+    class Sweepstakes
+    {
+        // member variables
+
+        // member methods
+        public Sweepstakes(string name)
+        {
+
+        }
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+        private string PickWinner()
+        {
+            return "winner";
+        }
+        private void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
